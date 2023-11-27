e040015137259a9b5c6150875323bcd37c56d79b
Brittany Moliver <britt.moliver@gmail.com>
2023-11-27 03:34:04 -0500

    Message Service done

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,3 +1,57 @@
+package service;
+
+import java.util.*;
+import dao.MessageDAO;
+import model.Message;
+
 public class MessageService {
-    
+
+    // add dependency
+    MessageDAO messageDAO;
+
+    public MessageService() {
+        this.messageDAO = new MessageDAO();
+    }
+
+    /*
+     * getMessage_id()
+     * getPosted_by()
+     * getMessage_text()
+     * getTime_posted_epoch
+     */
+
+    public Message addMessage(Message messageToAdd) {
+        return messageDAO.addMessage(messageToAdd.getPosted_by(), messageToAdd.getMessage_text());
+    }
+
+    public ArrayList<Message> getAllMessages() {
+        return messageDAO.getAllMessages();
+    }
+
+    public ArrayList<Message> getAllUserMessages(int posted_by) {
+        return messageDAO.getAllUserMessages(posted_by);
+    }
+
+    public Message getMessageById(int message_id) {
+        return messageDAO.getMessageById(message_id);
+    }
+
+    public Message updateMessageById(int message_id, String message_text) {
+        boolean result = messageDAO.updateMessageById(message_id, message_text);
+        if (result) {
+            return messageDAO.getMessageById(message_id);
+        } else {
+            return null;
+        }
+    }
+
+    public Message deleteMessageById(int message_id) {
+        boolean result = messageDAO.deleteMessageById(message_id);
+        if (result) {
+            return messageDAO.getMessageById(message_id);
+        } else {
+            return null;
+        }
+    }
+
 }
