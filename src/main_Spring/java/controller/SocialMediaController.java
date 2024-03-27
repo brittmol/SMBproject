5f6a26b22f3b657b75c3cc34b9699dedc34514cf
Brittany Moliver <britt.moliver@gmail.com>
2024-03-27 11:48:35 -0400

    fixed naming errors

--- src/main_Spring/java/controller/SocialMediaController.java
+++ src/main_Spring/java/controller/SocialMediaController.java
@@ -10,10 +10,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 /**
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
- * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
- * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
+ * TODO: You will need to write your own endpoints and handlers for your
+ * controller using Spring. The endpoints you will need can be
+ * found in readme.md as well as the test cases. You be required to use
+ * the @GET/POST/PUT/DELETE/etc Mapping annotations
+ * where applicable as well as the @ResponseBody and @PathVariable annotations.
+ * You should
+ * refer to prior mini-project labs and lecture materials for guidance on how a
+ * controller may be built.
  */
 @RestController
 @RequestMapping("/api")
@@ -51,8 +55,8 @@ public class SocialMediaController {
     }
 
     @PostMapping("/messages")
-    public Message addMessage(@RequestBody Message message) {
-        return messageService.addMessage(message);
+    public Message createMessage(@RequestBody Message message) {
+        return messageService.createMessage(message);
     }
 
     @PatchMapping("/messages/{id}")
