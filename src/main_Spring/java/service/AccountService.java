8c355c8def682ca0533a6b0f2e6c5faf57623fe7
Brittany Moliver <britt.moliver@gmail.com>
2024-03-27 10:34:24 -0400

    edited accountService

--- src/main_Spring/java/service/AccountService.java
+++ src/main_Spring/java/service/AccountService.java
@@ -5,6 +5,8 @@ import com.example.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
 public class AccountService {
 
@@ -13,13 +15,17 @@ public class AccountService {
 
     public Account register(Account account) {
         // Implement registration logic here
-        return accountRepository.save(account);
+        ArrayList<String> allAccounts = new ArrayList<>(accountRepository.getAllUsernames());
+        boolean usernameExists = allAccounts.contains(account.getUsername());
+        if(account.getUsername().length() > 0 && account.getPassword().length() >= 4 && !usernameExists) {
+            return accountRepository.createAccount(account);
+        }
+        return null;
     }
 
     public Account login(Account account) {
         // Implement login logic here
-        return accountRepository.findByUsernameAndPassword(account.getUsername(), account.getPassword());
+        return accountRepository.getAccount(account.getUsername(), account.getPassword());
     }
 
 }
-
